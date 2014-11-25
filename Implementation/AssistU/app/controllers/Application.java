3584da99fc1705d627942005c6835794e809202b
solix <soheil.jahanshahi@gmail.com>
2014-11-25 13:02:31 +0100

    UI login and dashboard is done

--- Implementation/AssistU/app/controllers/Application.java
+++ Implementation/AssistU/app/controllers/Application.java
@@ -6,9 +6,26 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-
+    /**
+     * responds to the index page request
+     * @return
+     */
     public static Result index() {
+        return ok(index.render("Dashboard"));
+    }
+    /**
+     * responds to the index page request
+     * @return
+     */
+    public static Result task() {
         return ok(task.render());
+    }
+    /**
+     * responds to the index page request
+     * @return
+     */
+    public static Result login() {
+        return ok(login.render());
     }
 
 }
