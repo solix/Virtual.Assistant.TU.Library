2d48460bd7310b5cc474902cb0574a90572f0b2a
solix <soheil.jahanshahi@gmail.com>
2014-11-25 21:13:19 +0100

    "basic calendar layout"

--- Implementation/AssistU/app/controllers/Application.java
+++ Implementation/AssistU/app/controllers/Application.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import play.*;
+
 import play.mvc.*;
 
 import views.html.*;
@@ -8,24 +8,34 @@ import views.html.*;
 public class Application extends Controller {
     /**
      * responds to the index page request
+     *
      * @return
      */
     public static Result index() {
         return ok(index.render("Dashboard"));
     }
+
     /**
      * responds to the index page request
+     *
      * @return
      */
     public static Result task() {
         return ok(task.render());
     }
+
     /**
      * responds to the index page request
+     *
      * @return
      */
     public static Result login() {
         return ok(login.render());
     }
 
+
+    public static Result calendar() {
+
+        return ok(calendar.render());
+    }
 }
