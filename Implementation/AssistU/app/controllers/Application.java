ddb342068e1ce535beef590ed4aaab73eada6383
solix <soheil.jahanshahi@gmail.com>
2014-11-30 23:33:41 +0100

    calendar refactoring is done

--- Implementation/AssistU/app/controllers/Application.java
+++ Implementation/AssistU/app/controllers/Application.java
@@ -39,7 +39,7 @@ public class Application extends Controller {
      */
     public static Result calendar() {
 
-        return ok(calendar.render());
+        return ok(calendar.render("My Calendar"));
     }
 
     /**
