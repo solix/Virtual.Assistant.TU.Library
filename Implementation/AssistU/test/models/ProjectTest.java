864b44b12112d1002fae68e244fd16a500ab4a79
Arnaud <a.hambenne@gmail.com>
2014-12-12 13:13:40 +0100

    Project test updated with persistent DB

--- Implementation/AssistU/test/models/ProjectTest.java
+++ Implementation/AssistU/test/models/ProjectTest.java
@@ -24,20 +24,20 @@ public class ProjectTest extends WithApplication {
 
     @Test
     public void createAndArchiveProject(){
-        Project.create("Thesis", "first thesis", "arnaud@assistu.nl");
+        Project.create("Thesis", "first thesis", "ah@gmail.com", "nanana");
         Project T = Project.find.where().eq("folder" , "Thesis").findUnique();
         assertNotNull(T);
         assertEquals("Thesis" , T.folder);
         assertEquals("first thesis" , T.name);
-        assertEquals(true , T.userlist.contains(User.find.byId("arnaud@assistu.nl")));
-        Application.archiveProject("arnaud@assistu.nl", T.id);
+        assertEquals(true , T.users.contains(User.find.byId("ah@gmail.com")));
+        Application.archiveProject("ah@gmail.com", T.id);
         Project Td = Project.find.where().eq("folder" , "Thesis").findUnique();
         assertEquals(Td.active, false);
     }
 
     @Test
     public void createAndEditProject(){
-        Project.create("Thesis", "first thesis", "arnaud@assistu.nl");
+        Project.create("Thesis", "first thesis", "ah@gmail.com", "hahaha");
         Project T = Project.find.where().eq("folder" , "Thesis").findUnique();
         assertNotNull(T);
         assertEquals("Thesis" , T.folder);
@@ -45,7 +45,7 @@ public class ProjectTest extends WithApplication {
         Project Tu = Project.find.where().eq("name" , "first thesis").findUnique();
         assertEquals(Tu.folder, "Report");
         assertEquals("Report", Project.find.byId(T.id).folder);
-        assertTrue(Project.find.where().eq("folder", "Report").findUnique().userlist.contains(User.find.byId("arnaud@assistu.nl")));
+        assertTrue(Project.find.where().eq("folder", "Report").findUnique().users.contains(User.find.byId("ah@gmail.com")));
     }
 
     /**
@@ -54,15 +54,15 @@ public class ProjectTest extends WithApplication {
      */
     @Test
     public void projectScene1(){
-        Project thesis = Project.create("Thesis", "Final Thesis", "arnaud@assistu.nl");
+        Project thesis = Project.create("Thesis", "Final Thesis", "ah@gmail.com", "booboo");
         assertEquals(thesis.name, "Final Thesis");
-        Project.addMemberAs(thesis.id, "soheil@assistu.nl");
-        Application.removeMemberFromProject(thesis.id, "arnaud@assistu.nl");
-        //You need to reinstantiate the same project again, if you don't the test will fail
+        Project.addMemberAs(thesis.id, "soli@gmail.com");
+        Application.removeMemberFromProject(thesis.id, "ah@gmail.com");
+        //TODO: You need to reinstantiate the same project again, if you don't the test will fail, need to find out whether this is a bug or expected behaviour
         Project updated = Project.find.byId(thesis.id);
-        assertFalse(updated.userlist.contains(User.find.byId("arnaud@assistu.nl")));
-        assertTrue(updated.userlist.contains(User.find.byId("soheil@assistu.nl")));
-        assertTrue(updated.userlist.size() == 1);
+        assertFalse(updated.users.contains(User.find.byId("ah@gmail.com")));
+        assertTrue(updated.users.contains(User.find.byId("soli@gmail.com")));
+        assertTrue(updated.users.size() == 1);
     }
 
     /**
