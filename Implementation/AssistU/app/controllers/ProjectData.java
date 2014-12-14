75fac96aab8633e3db690236287218e18d9938fa
Arnaud <a.hambenne@gmail.com>
2014-12-14 17:10:31 +0100

    Added Comments and TODOs to new projectdata controller

--- Implementation/AssistU/app/controllers/ProjectData.java
+++ Implementation/AssistU/app/controllers/ProjectData.java
@@ -12,6 +12,11 @@ public class ProjectData extends Controller {
 
     static Form<Project> projectForm = Form.form(Project.class);
 
+    /**
+     * TODO: Adapt this function after login is implemented and the current user is known
+     * This function creates a new Project initiated by a user that automatically becomes its owner.
+     * @return
+     */
     public static Result createProject() {
         User user= User.find.where().eq("email", "alex@gmail.com").findUnique();
         Form<Project> filledProjectForm = projectForm.bindFromRequest();
@@ -24,6 +29,12 @@ public class ProjectData extends Controller {
         }
     }
 
+    /**
+     * TODO: See if there is a way to only pass values that need to be changed (Map?)
+     * This function edits a project with values catched from a form.
+     * @param pid: The Project ID of the Project that needs it details edited
+     * @return
+     */
     public static Result editProject(Long pid) {
         Form<Project> filledProjectForm = projectForm.bindFromRequest();
         if(filledProjectForm.hasErrors()) {
@@ -35,6 +46,13 @@ public class ProjectData extends Controller {
 
     }
 
+    /**
+     * This function refers to the model's archive function. The unused uid is to check whether the person is an
+     * owner of the project at hand (future implementation)
+     * @param uid: The User ID of the person requesting for archiving
+     * @param pid: The Project ID of the project that is up for archiving
+     * @return
+     */
     public static Result archiveProject(String uid, Long pid) {
         Project.archive(pid);
         return redirect(routes.Application.project());
@@ -42,8 +60,9 @@ public class ProjectData extends Controller {
 
     /**
      * TODO: Need to add third Role parameter
-     *
-     * @param pid
+     * This function catches the Dynamicform from the template with the ID of the User that needs to
+     * be added to the project of which the Project ID has been passed
+     * @param pid: The project to which the user in the form has to be assigned
      * @return
      */
     public static Result addMemberToProjectAs(Long pid){
@@ -54,8 +73,11 @@ public class ProjectData extends Controller {
 
     /**
      * TODO: Only owners should be able to remove non-owners
-     * @param uid
-     * @param pid
+     * This function removes a user from a project. This function doubles
+     * as removing someone and leaving yourself. In the latter case your
+     * own User ID is passed.
+     * @param uid: The ID of the User that is being removed
+     * @param pid: The ID of the Project in which the User had to be removed from
      * @return
      */
     public static Result removeMemberFromProject(String uid, Long pid){
