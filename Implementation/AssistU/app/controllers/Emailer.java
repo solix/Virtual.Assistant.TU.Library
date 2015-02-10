b9a5582c3164faa0bce9c9fe23464ca8d526eb5a
solix <soheil.jahanshahi@gmail.com>
2015-02-10 22:45:52 +0100

    emaol is connected and can be sent

--- Implementation/AssistU/app/controllers/Emailer.java
+++ Implementation/AssistU/app/controllers/Emailer.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import com.feth.play.module.pa.PlayAuthenticate;
+import models.User;
 import org.apache.commons.mail.EmailAttachment;
+import play.Logger;
 import play.Play;
 import play.libs.mailer.Email;
 import play.libs.mailer.MailerPlugin;
@@ -9,19 +12,23 @@ import play.mvc.Result;
 
 import java.io.File;
 
+/**
+ * attention: in case you are using intellij idea you might see lots of errors , don't pay attention it works fine
+ */
 public class Emailer extends Controller {
 
-    public static Result send() {
+    public static Result sendWelcomeMessage() {
+        User user = User.findByAuthUserIdentity(PlayAuthenticate.getUser(session()));
+
         final Email email = new Email();
-        email.setSubject("Simple email");
+        email.setSubject("Welcome to AssisTU");
         email.setFrom("soheil.jahanshahi@gmail");
-        email.addTo("soheil.jahanshahi@gmail.com");
-        email.addAttachment("favicon.png", new File(Play.application().classloader().getResource("public/images/favicon.png").getPath()));
-        email.addAttachment("data.txt", "data".getBytes(), "text/plain", "Simple data", EmailAttachment.INLINE);
-        email.setBodyText("A text message");
-        email.setBodyHtml("<html><body><p>An <b>html</b> message</p></body></html>");
-        String id = MailerPlugin.send(email);
-        return ok("Email " + id + " sent!");
+        email.addTo(user.email);
+        email.setBodyText("Good day"+ user.name);
+        email.setBodyHtml(views.html.email.testemail.render(user).toString());
+        MailerPlugin.send(email);
+        Logger.info("an email has been sent to "+ user.email);
+        return ok("Email " + " sent!");
     }
 
 }
