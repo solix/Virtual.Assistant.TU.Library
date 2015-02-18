5f78f559dec4aa079cb91bdce4e3dc131130a83c
Arnaud <a.hambenne@gmail.com>
2015-02-18 09:42:57 +0100

    updated suggestiondata

--- Implementation/AssistU/app/controllers/SuggestionData.java
+++ Implementation/AssistU/app/controllers/SuggestionData.java
@@ -13,21 +13,21 @@ public class SuggestionData extends Controller{
     public static Result suggestion(String subject) {
         User user = User.findByAuthUserIdentity(PlayAuthenticate.getUser(session()));
         if(user != null){
-            if(subject.equals("Orientation")){
-                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-eye", user));
-            } else if(subject.equals("Exploration")){
-                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-arrows-alt", user));
-            } else if(subject.equals("Processing")){
-                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-sitemap", user));
-            } else if(subject.equals("Literature Search")){
-                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-book", user));
-            } else if(subject.equals("Publish")){
-                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-newspaper-o", user));
-            } else if(subject.equals("Keeping Up-to-date")){
-                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-refresh", user));
-            } else {
+//            if(subject.equals("Orientation")){
+//                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-eye", user));
+//            } else if(subject.equals("Exploration")){
+//                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-arrows-alt", user));
+//            } else if(subject.equals("Processing")){
+//                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-sitemap", user));
+//            } else if(subject.equals("Literature Search")){
+//                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-book", user));
+//            } else if(subject.equals("Publish")){
+//                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-newspaper-o", user));
+//            } else if(subject.equals("Keeping Up-to-date")){
+//                return ok(suggestions_tagcloud.render("Suggestions", subject, "fa-refresh", user));
+//            } else {
                 return Application.suggestions();
-            }
+//            }
 
         }
         else
