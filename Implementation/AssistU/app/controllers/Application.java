a5a0b63c4a45a2bcaa86418415c200d1ede6466e
Arnaud <a.hambenne@gmail.com>
2015-02-19 10:23:21 +0100

    fixed relogin

--- Implementation/AssistU/app/controllers/Application.java
+++ Implementation/AssistU/app/controllers/Application.java
@@ -25,7 +25,7 @@ public class Application extends Controller {
         if (route != null) {
             return redirect(route);
         }
-        return Authentication.login();
+        return Application.index();
     }
 
     /**
